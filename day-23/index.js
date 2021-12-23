483c31bf7d3872ddeeb10a1a60f6cc44669f2319
Ken Gorab <ken.gorab@gmail.com>
2021-12-23 09:18:52 -0500

    Day 23 (plus day 21 in Abra)

---
+++ day-23/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const _ = require('lodash')
+
+const realInput = fs.readFileSync('./input.txt', { encoding: 'utf-8' })
+const demoInput = `
+
+`
+
+function processInput(input) {
+  return input.trim().split('\n').map(l => {
+    // Process input
+  })
+}
+
+function part1() {
+  // Good luck!
+}
+console.log(part1())
